fd1e02c31e4f483d754dbc6f6b338a1e216b527b
im-siu <geun0404@pukyong.ac.kr>
2026-08-25 16:51:43 +0900

    Initial Xcode project

---
+++ MyLv/MyLvApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLvApp.swift
+//  MyLv
+//
+//  Created by Im gaeun on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct MyLvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
